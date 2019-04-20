dfe7333f93655fe06038c20ebadc195346bc83d1
Doraemonzzz <doraemon_zzz@163.com>
2019-04-20 09:38:34 +0800

    更新作业8

--- hw8/代码/Problem 11-18.py
+++ hw8/代码/Problem 11-18.py
@@ -1,33 +1,52 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  5 11:38:49 2018
+Created on Fri Apr 19 16:22:57 2019
 
-@author: Administrator
+@author: qinzhen
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+#读取数据
 train = np.loadtxt("hw8_train.dat")
 test = np.loadtxt("hw8_test.dat")
+X_train, y_train = train[:, :-1], train[:, -1]
+X_test, y_test = test[:, :-1], test[:, -1]
 
-def Knn(x, data, k):
-    distance = np.sum((x[:-1] - data[:, :-1])**2, axis = 1)
-    index = np.argsort(distance)[:k]
-    label = data[:, -1][index]
-    return np.sign(np.sum(label))
-
-def Error(data1, data2 ,k):
-    error = 0
-    for x in data1:
-        #判断label是否相等
-        error += Knn(x, data2, k) != x[-1]
-    return error/data1.shape[0]
+class KNeighborsClassifier_():
+    def __init__(self, n_neighbors):
+        self.n_neighbors = n_neighbors
+        
+        
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        
+    def predict(self, X):
+        #计算距离矩阵
+        d1 = np.sum(X ** 2, axis=1).reshape(-1, 1)
+        d2 = np.sum(self.X ** 2, axis=1).reshape(1, -1)
+        dist = d1 + d2 - 2 * X.dot(self.X.T)
+        
+        #找到最近的k个点的索引
+        index = np.argsort(dist, axis=1)[:, :self.n_neighbors]
+        #计算预测结果
+        y = np.sign(np.sum(self.y[index], axis=1))
+        
+        return y
 
 # Q11-12
 K = [1, 3, 5, 7, 9]
 Ein = []
 for k in K:
-    Ein.append(Error(train, train, k))
+    #训练模型
+    knn = KNeighborsClassifier_(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    #预测结果
+    y = knn.predict(X_train)
+    ein = np.mean(y != y_train)
+    Ein.append(ein)
 
 plt.scatter(K, Ein)
 plt.title("$K$ VS $E_{in}$")
@@ -39,7 +58,13 @@ plt.show()
 K = [1, 3, 5, 7, 9]
 Eout = []
 for k in K:
-    Eout.append(Error(test, train, k))
+    #训练模型
+    knn = KNeighborsClassifier_(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    #预测结果
+    y = knn.predict(X_test)
+    eout = np.mean(y != y_test)
+    Eout.append(eout)
 
 plt.scatter(K, Eout)
 plt.title("$K$ VS $E_{out}$")
@@ -48,26 +73,42 @@ plt.ylabel("$E_{out}$")
 plt.show()
 
 
-def Knn_new(x, data, gamma):
-    distance = np.sum((x[:-1] - data[:, :-1])**2, axis = 1)
-    Exp = np.exp(-gamma * distance)
-    S = data[:, -1] * Exp
-    return np.sign(np.sum(S))
+class RBFNetworkClassifier():
+    def __init__(self, gamma):
+        self.gamma = gamma
+        self.beta = None
 
-def Error_new(data1, data2, gamma):
-    error = 0
-    for x in data1:
-        #判断label是否相等
-        error += Knn_new(x, data2, gamma) != x[-1]
-    return error/data1.shape[0]
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        
+    def predict(self, X):
+        #计算距离矩阵
+        d1 = np.sum(X ** 2, axis=1).reshape(-1, 1)
+        d2 = np.sum(self.X ** 2, axis=1).reshape(1, -1)
+        dist = d1 + d2 - 2 * X.dot(self.X.T)
+        
+        #计算exp(-gamma*dist)
+        d = np.exp(-self.gamma * dist)
+        
+        #计算预测结果
+        y = np.sign(np.sum(d * self.y, axis=1))
+        
+        return y
 
 # Q15-16
 Gamma = [0.001, 0.1, 1, 10, 100]
-Ein_new = []
+Ein_rbf = []
 for gamma in Gamma:
-    Ein_new.append(Error_new(train, train, gamma))
+    #训练模型
+    knn = RBFNetworkClassifier(gamma=gamma)
+    knn.fit(X_train, y_train)
+    #预测结果
+    y = knn.predict(X_train)
+    ein = np.mean(y != y_train)
+    Ein_rbf.append(ein)
 
-plt.scatter(Gamma, Ein_new)
+plt.scatter(Gamma, Ein_rbf)
 plt.title("$\gamma$ VS $E_{in}$")
 plt.xlabel("$\gamma$")
 plt.ylabel("$E_{in}$")
@@ -75,22 +116,18 @@ plt.show()
 
 # Q17-18
 Gamma = [0.001, 0.1, 1, 10, 100]
-Eout_new = []
+Eout_rbf = []
 for gamma in Gamma:
-    Eout_new.append(Error_new(test, train, gamma))
+    #训练模型
+    knn = RBFNetworkClassifier(gamma=gamma)
+    knn.fit(X_train, y_train)
+    #预测结果
+    y = knn.predict(X_test)
+    eout = np.mean(y != y_test)
+    Eout_rbf.append(eout)
 
-plt.scatter(Gamma, Eout_new)
+plt.scatter(Gamma, Eout_rbf)
 plt.title("$\gamma$ VS $E_{out}$")
 plt.xlabel("$\gamma$")
 plt.ylabel("$E_{out}$")
 plt.show()
-
-'''
-data = train
-k = 3
-x = train[0]
-distance = np.sum((x[:-1] - data[:, :-1])**2, axis = 1)
-index = np.argsort(distance)[:k]
-label = data[:, -1][index]
-np.sign(np.sum(label))
-'''
--- hw8/代码/Problem 19-20.py
+++ hw8/代码/Problem 19-20.py
@@ -1,49 +1,101 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  5 12:03:28 2018
+Created on Fri Apr 19 16:24:00 2019
 
-@author: Administrator
+@author: qinzhen
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-train = np.loadtxt("hw8_nolabel_train.dat")
+X = np.genfromtxt("hw8_nolabel_train.dat")
 
-def K_mean(data, k):
-    data = train
-    N = data.shape[0]
-    #初始化中心
-    index = np.random.choice(range(N), size=k, replace=False )
-    center = data[index]
-    newcenter = np.copy(center)
-    label = np.zeros(N)
-    while True:
-        #Step 1:计算距离最近的中心
-        for i in range(N):
-            distance = np.sum((data[i] - center)**2, axis = 1)
-            label[i] = np.argmin(distance)
-        #Step 2:计算新的中心
-        for i in range(k):
-            newcenter[i] = np.mean(data[label == i])
-        #判断是否收敛
-        if np.sum((newcenter - center)**2) < 1e-5:
-            break
-        center = np.copy(newcenter)
-    
-    #计算误差
-    Error = 0
-    for i in range(k):
-        Error += np.sum((data[label == i] - center[i])**2)
-    return Error/N
+class KMeans_():
+    def __init__(self, k, D=1e-5):
+        #聚类数量
+        self.k = k
+        #聚类中心
+        self.cluster_centers_ = []
+        #聚类结果
+        self.labels_ = []
+        #设置阈值
+        self.D = D
+        
+    def fit(self, X):
+        #数据维度
+        n, d = X.shape
+        #聚类标签
+        labels = np.zeros(n, dtype=int)
+        #初始中心点
+        index = np.random.randint(0, n, self.k)
+        cluster_centers = X[index]
+        #记录上一轮迭代的聚类中心
+        cluster_centers_pre = np.copy(cluster_centers)
+        
+        while True:
+            #计算距离矩阵
+            d1 = np.sum(X ** 2, axis=1).reshape(-1, 1)
+            d2 = np.sum(cluster_centers ** 2, axis=1).reshape(1, -1)
+            dist = d1 + d2 - 2 * X.dot(cluster_centers.T)
+            
+            #STEP1:找到最近的中心
+            labels = np.argmin(dist, axis=1)
+            #STEP2:重新计算中心
+            for i in range(self.k):
+                #第i类的索引
+                index = (labels==i)
+                #第i类的数据
+                x = X[index]
+                #判断是否有点和某聚类中心在一类
+                if len(x) != 0:
+                    cluster_centers[i] = np.mean(x, axis=0)
+                
+            #计算误差
+            delta = np.linalg.norm(cluster_centers - cluster_centers_pre)
+            
+            if delta < self.D:
+                break
+            
+            cluster_centers_pre = np.copy(cluster_centers)
+            
+        self.cluster_centers_ = np.copy(cluster_centers)
+        self.labels_ = labels
+        
+        
+    def predict(self, X):
+        #计算距离矩阵
+        d1 = np.sum(X ** 2, axis=1).reshape(-1, 1)
+        d2 = np.sum(self.cluster_centers_ ** 2, axis=1).reshape(1, -1)
+        dist = d1 + d2 - 2 * X.dot(self.cluster_centers_.T)
+        
+        #找到最近的中心
+        self.cluster_centers_ = np.argmin(dist, axis=1)
+        
+        return self.cluster_centers_
 
+n = X.shape[0]
 K = [2, 4, 6, 8, 10]
 Ein = []
 for k in K:
-    Ein.append(K_mean(train, k))
+    #训练模型
+    kmeans = KMeans_(k)
+    kmeans.fit(X)
+    #获得标签
+    label = kmeans.labels_
+    #获得聚类中心
+    center = kmeans.cluster_centers_
+    #计算Ein
+    ein = 0
+    for i in range(k):
+        #计算每一类的误差
+        ein += np.sum((X[label==i] - center[i]) ** 2)
+    #计算均值
+    ein /= n
+    Ein.append(ein)
     
 plt.scatter(K, Ein)
 plt.title("$K$ VS $E_{in}$")
 plt.xlabel("$K$")
 plt.ylabel("$E_{in}$")
 plt.show()
+
 
